c1dca73b7300e11b83da2669bd24c537f0860fd9
Dave Bland <david.bland@gmx.co.uk>
2019-05-22 17:47:15 +0100

    js to manage api form processing, including jQuery

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # The S3 Log Analyser
 
-Interactive Frontend Development Milestone Project for Code Institute
+Interactive Frontend Development Milestone Project for Code Institute.
+
+A web application to retrieve two common types of log files stored in Amazon's Web Services (AWS) object storage system (S3). The data is access records for an S3 bucket or CloudFront distribution and the app visualises it for the user as charts & tables.
 
 ## Demo
 
 ## UX
 
+This application makes it quick and easy for a user to connect to thier AWS S3 bucket and analyse access logs stored there by way of charts & tables. It is a single page website.
+
 ### User Stories
 
+1. The purpose of the application is clear.
+2. The user interface is intuitive enough that an IT literate user with knowledge of AWS can use it without looking at the help documentation.
+3. A user who is unsure what credentials are required can find links to relevant tutorials in the help modal.
+4. The charts & tables are relevant to the purpose of presenting the data.
+5. Where data values are missing in the log files, these are presented gracefully on the charts.
+6. If a S3 bucket doesn't contain correctly formatted log files the application fails gracefully with a relevant error message.
+7. Large data sets load fast enough to be usable or fail gracefully.
+
 ## Features
+
+- Locally saved API credentials
 
 ### Future Additions
 
@@ -18,6 +32,7 @@ Interactive Frontend Development Milestone Project for Code Institute
 - CSS3
 - Libraries
     - Bootstrap (4.3.1)
+    - jQuery (3.4.1)
 
 ## Testing
 
@@ -26,6 +41,8 @@ Interactive Frontend Development Milestone Project for Code Institute
 ### User Story Testing
 
 ## Deployment
+
+Scripts loaded after HTML via defer attribute.
 
 ## Credits
 
@@ -37,5 +54,6 @@ Interactive Frontend Development Milestone Project for Code Institute
 
 - wireframe.cc (online wireframing tool)
 - Bootstrap official documentation
+- jQuery officail documentation
 - HTML5 validator https://validator.w3.org/
 - CSS3 validator https://jigsaw.w3.org/css-validator/
---
+++ scripts/forms.js
@@ -0,0 +1,210 @@
+/* 
+    THE S3 LOG ANALYSER
+    FORM MANAGEMENT
+    BY DAVID BLAND
+*/
+
+// Application wide variables
+var awsCreds = {};
+var saveCredsFlag = false;
+
+// Initial functions to run
+checkSavedCredsButtonState();
+getAwsRegions();
+
+/* FORM DISPLAY */
+
+// Enable/disable API Credentials form
+function enableCredsForm(yes) {
+    if (yes) {
+        //Enable form fields & buttons
+        $('#form-api-creds :input').prop('disabled', false);
+        $('#buttons-load-saved :input').prop('disabled', false);
+        // Check saved creds button state having just enabled it
+        checkSavedCredsButtonState();
+    } else {
+        //Disable form fields & buttons
+        $('#form-api-creds :input').prop('disabled', true);
+        $('#buttons-load-saved :input').prop('disabled', true);
+    }
+}
+
+// Enable/disable Filter Logs form
+function enableFilterForm(yes) {
+    if (yes) {
+        //Enable form fields & buttons
+        $('#form-filter-log-files :input').prop('disabled', false);
+    } else {
+        //Disable form fields & buttons
+        $('#form-filter-log-files :input').prop('disabled', true);
+    }
+}
+
+// Reset API Credentials Form
+function resetCredsForm() {
+    $('#form-api-creds')[0].reset(); 
+}
+
+// Reset Filter Logs Form
+function resetFilterForm() {
+    $('#form-filter-log-files')[0].reset();
+}
+
+// Clear API Connect message area
+function clearApiMessageArea() {
+    $('#message-area-api-connect').empty();
+}
+
+// Clear Load Logs message area
+function clearLoadLogsMessageArea() {
+    $('#message-area-load-logs').empty();
+}
+
+// Reset Page
+function resetPage() {
+    // Reset forms
+    resetCredsForm();
+    resetFilterForm();
+    // Disable filter form and enable creds form
+    enableCredsForm(true);
+    enableFilterForm(false);
+    // Clear message areas
+    clearApiMessageArea();
+    clearLoadLogsMessageArea()
+}
+
+/* AWS REGION DROPDOWN */
+
+// Load list of AWS regions into creds form
+function getAwsRegions() {
+    let awsRegions = [
+        "us-east-2",
+        "us-east-1",
+        "us-west-1",
+        "us-west-2",
+        "ap-east-1",
+        "ap-south-1",
+        "ap-northeast-2",
+        "ap-southeast-1",
+        "ap-southeast-2",
+        "ap-northeast-1",
+        "ca-central-1",
+        "cn-north-1",
+        "cn-northwest-1",
+        "eu-central-1",
+        "eu-west-1",
+        "eu-west-2",
+        "eu-west-3",
+        "eu-north-1",
+        "sa-east-1",
+        "us-gov-east-1",
+        "us-gov-west-1",
+    ]
+    // For each region in the array add an option element to the form dropdown
+    awsRegions.forEach(function(region) {
+        $('#aws-region-select').append(`<option value="${region}">${region}</option>`);
+    });
+    // Pre select 'eu-west-2' as region
+    $('#aws-region-select option[value="eu-west-2"]').attr("selected", true);
+}
+    
+
+/* FORM SUBMISSION */
+
+// Triggered on submission of API credentials form
+function submitCredsForm(apiCreds) {
+
+    // Disable creds form whilst processing
+    enableCredsForm(false);
+
+    // Write loading to message area
+    $('#message-area-api-connect').append('<p>Loading...</p>');
+
+    // Update object holding AWS Creds
+    awsCreds = {
+        awsRegion: apiCreds.awsRegion.value,
+        keyId: apiCreds.keyId.value,
+        keySecret: apiCreds.keySecret.value,
+        bucketName: apiCreds.bucketName.value
+    }
+    console.table(awsCreds);
+
+    // Invoke API list objects and manage promise
+    awsListObjects().then(function(success) {
+        // On list function success
+        $('#message-area-api-connect').append(`<p>${success}</p>`);
+        // Enable filter form
+        enableFilterForm(true);
+    }, function(error) {
+        // On error write details to message area
+        $('#message-area-api-connect').append(`<p>ERROR: ${error.code} - ${error.message}</p>`);
+        // Enable creds form again & clear message area
+        enableCredsForm(true);
+        clearApiMessageArea();       
+    });
+
+    // Save creds
+    saveCreds();
+
+    // Stop default form submission (don't reload page)
+    return false;
+}
+
+/* SAVED CREDENTIALS */
+
+// Save credentials (if flag set)
+function saveCreds() {
+    if (saveCredsFlag) {
+        // Save credentials locally with property name to match those in awsCreds object
+        for (var property in awsCreds) {
+            localStorage.setItem(property, awsCreds[property]);    
+        }        
+    }
+}
+
+// Load saved credentials
+function loadSavedCreds() {
+    // Overwrite creds object
+    awsCreds = {
+        awsRegion: localStorage.getItem('awsRegion'),
+        keyId: localStorage.getItem('keyId'),
+        keySecret: localStorage.getItem('keySecret'),
+        bucketName: localStorage.getItem('bucketName')
+    }
+    // Input data into form fields
+    $('#access-key-id').val(awsCreds.keyId);
+    $('#access-key-secret').val(awsCreds.keySecret);
+    $('#aws-region-select').val(awsCreds.awsRegion);
+    $('#bucket-name').val(awsCreds.bucketName);
+}
+
+// Clear saved credentials
+function clearSavedCreds() {    
+    localStorage.clear();
+    // Check saved creds button state to disable
+    checkSavedCredsButtonState();
+}
+
+// Check if saved credentials are available and enable button accordingly
+function checkSavedCredsButtonState() {    
+    if (localStorage.length > 0) {
+        $('#buttons-load-local-saved :input').prop('disabled', false);
+    } else {
+        $('#buttons-load-local-saved :input').prop('disabled', true);
+    }
+}
+
+/* DEMO CREDENTIALS */
+
+//TEMP
+
+function awsListObjects() {
+    result = new Promise(function(resolve, reject) {
+        if(awsCreds.keyId == 'test') {
+            resolve('Creds Recieved');
+        } else {
+            reject({ code: 'Wrong creds', message: 'error details'});
+        };
+    });
+    return result;
+}
