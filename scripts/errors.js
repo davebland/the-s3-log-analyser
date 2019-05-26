f7d8458778ff60b10d4dbae4098689271f4a179b
Dave Bland <david.bland@gmx.co.uk>
2019-05-26 20:11:26 +0100

    updated error button continue functionality

--- scripts/errors.js
+++ scripts/errors.js
@@ -12,7 +12,7 @@ function displayErrors() {
         if (error.severity == 'fatal') {
             // Show fatal errors & disable continue button
             $('#modal-error-message-area').append(`${error.type}<div class="alert alert-danger" role="alert">${error.errorMessage}</div>`);
-            $('#button-error-modal-continue').prop('disabled', true);
+            $('#button-error-modal-continue:input').prop('disabled', true);
         } else {
             // Assume warning
             $('#modal-error-message-area').append(`${error.type}<div class="alert alert-warning" role="alert">${error.errorMessage}</div>`);
